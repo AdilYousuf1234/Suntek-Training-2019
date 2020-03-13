89f57b176cd9fbbcf2636f8230e34e2bf23e45df
Mohammed Adil Yousuf <adilyousuf1234@gmail.com>
2020-03-13 21:37:33 +0530

    Update AverageCase.py

--- March 2020/BitManipulation Q6/AverageCase.py
+++ March 2020/BitManipulation Q6/AverageCase.py
@@ -1,14 +1,14 @@
 def Solution():
-    d=dict()
-    l=list(map(int,input().split()))
-    for i in range(len(l)):
-        if(l[i] not in d):
-            d[l[i]]=0
-        else:
-            d[l[i]]+=1
+    d=dict()                            #create a dictionary
+    l=list(map(int,input().split()))    #input
+    for i in range(len(l)):             #
+        if(l[i] not in d):              #if elements is not a key in dictionary
+            d[l[i]]=0                   #initialize with 0
+        else:                           #else
+            d[l[i]]+=1                  #increment the value
     #print(d)
-    for i in d:
-        if(d.get(i)==0):
-            print(i)
+    for i in d:                         #
+        if(d.get(i)==0):                #if value is equal to 0
+            print(i)                    #the key is answer
             break
 Solution()
